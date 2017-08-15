3513672f88e5a2b78346bd6bc11ffd23169f9f78
pierfonda <pierfonda@gmail.com>
2017-08-15 11:18:29 +0200

    last version before holiday

--- movie_3D.py
+++ movie_3D.py
@@ -20,10 +20,11 @@ for i in range(256):
 
 f=listconf[0]
 phi = np.transpose(np.loadtxt(f))
-mlab.figure(bgcolor=(0,0,0))
+mlab.figure(bgcolor=(1,1,1))
 s = mlab.triangular_mesh(coord3D[0],coord3D[1],coord3D[2],triangles,scalars = (phi[0]+1)/2,vmax=1,vmin=0,colormap='PiYG')
 #s.module_manager.scalar_lut_manager.lut.table = rgmap
-mlab.view(45, -45,2.1)
+mlab.view(90, 0, 2)
+#mlab.view(45, -45,2.1)
 n=f.split("_")[1]
 n=n.split(".")[0]
 mlab.savefig("t_"+n+".png",size=(1920, 1080))
